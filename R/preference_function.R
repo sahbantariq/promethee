34f225a41c454f75386e37e32c865c5f253e4008
sahbantariq <sehbantariq@gmail.com>
2017-04-30 23:01:13 +0300

    Add all criterion preference functions

--- R/preference_function.R
+++ R/preference_function.R
@@ -7,12 +7,28 @@
 #'
 #' @export
 pref_func <- function(row_wise_difference,
-                      preference_function = "simple") {
+                      preference_function = "simple", p = 1,
+                      q = 0 , s = 0.5) {
 
-  if (preference_function == "simple") {
-    row_wise_difference %>%
-      dplyr::mutate_at(c(-1,-2),
-                       function(x) ifelse(x > 0, x, 0))
-  }
+  gaussian <- exp(1)^(-(d^2)/2*(s^2))
+
+  switch(preference_function,
+         "simple" = dplyr::mutate_at(
+           row_wise_difference, c(-1,-2), function(x) ifelse(x > 0, x, 0)),
+         "usual" = dplyr::mutate_at(
+           row_wise_difference, c(-1,-2), function(x) ifelse(x <= 0, 0, 1)),
+         "u-shape" = dplyr::mutate_at(
+           row_wise_difference, c(-1,-2), function(x) ifelse(x <= q, 0, 1)),
+         "v-shape" = dplyr::mutate_at(
+           row_wise_difference, c(-1,-2), function(x) ifelse(
+             x <= 0, 0, ifelse((x >= 0 & x <= p), d/p, 1))),
+         "level" = dplyr::mutate_at(
+           row_wise_difference, c(-1,-2), function(x) ifelse(
+             x <= q, 0, ifelse((x > q & x <= p), 1/2, 1))),
+         "v-shape-indifference" = dplyr::mutate_at(
+           row_wise_difference, c(-1,-2), function(x) ifelse(
+             x <= q, 0, ifelse((x > q & x <= p), (d-q)/(p-q), 1))),
+         "gaussian" = dplyr::mutate_at(
+           row_wise_difference, c(-1,-2),
+           function(x) ifelse(x <= 0, 0, gaussian)))
 }
-
